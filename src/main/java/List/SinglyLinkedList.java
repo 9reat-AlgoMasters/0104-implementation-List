676a81f5b828cd38ca60e094ff357cedc9e36862
An Na <99172832+An0401na@users.noreply.github.com>
2023-02-21 20:30:42 +0900

    impl : SinglyLinkedList
    remove(), add(), removeFirst(), isEmpty() 미완성

--- src/main/java/List/SinglyLinkedList.java
+++ src/main/java/List/SinglyLinkedList.java
@@ -1,51 +1,153 @@
 package List;
 
+import exceptions.CustomIndexOutOfBoundsException;
+import exceptions.CustomNoSuchElementException;
+
+class Node{
+    private int data;
+    public Node next;
+    public Node(){}
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    public Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    public int getData() {
+        return data;
+    }
+}
 public class SinglyLinkedList implements List {
+    private int size;
+    private Node head;
+    private Node tail;
+
+
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
     
     @Override
     public boolean isEmpty() {
-        return false;
+        if(head == null){
+            return true;
+        }
+        return  true;
     }
     
     @Override
     public boolean contains(int num) {
+        Node n = head;
+        while (!(n == null)){
+            if(num == n.getData()){
+                return true;
+            }else{
+                n = n.next;
+            }
+        }
         return false;
     }
     
     @Override
-    public int get(int index) {
-        return 0;
+    public int get(int index) throws  CustomIndexOutOfBoundsException{
+        if (index>=size || index<0){
+            throw new CustomIndexOutOfBoundsException();
+        }
+        Node n = head;
+        for (int i = 0; i < index; i++) {
+            n = n.next;
+        }
+        return n.getData();
     }
     
-    public int getFirst() {
-        return 0;
+    public int getFirst() throws CustomNoSuchElementException{
+        if (size==0){
+            throw new CustomNoSuchElementException();
+        }
+        return head.getData();
     }
     
     @Override
     public void add(int num) {
-    
+        Node newN = new Node(num);
+        Node n = head;
+        if(n == null){
+            head = newN;
+            size+=1;
+            return;
+        }
+        while (n.next != null){
+            n = n.next;
+        }
+        n.next = newN;
+        size +=1;
     }
     
     @Override
-    public void add(int index, int num) {
-    
+    public void add(int index, int num) throws CustomIndexOutOfBoundsException{
+        if(index > size || index <0){
+            throw new CustomIndexOutOfBoundsException();
+        }
+        if(head == null){
+            Node n = new Node(num);
+            head = n;
+            return;
+        }
+        Node pre = head;
+        for (int i = 0; i < index-1; i++) {
+            pre = pre.next;
+        }
+        Node n = new Node(num, pre.next);
+        pre.next = n;
+        size +=1;
+
     }
     
     @Override
-    public int remove(int index) {
-        return 0;
+    public int remove(int index) throws CustomIndexOutOfBoundsException{
+        if(index >= size || index < 0){
+            throw new CustomIndexOutOfBoundsException();
+        }
+        int data;
+
+        if(index ==0){
+            data = head.getData();
+            head = null;
+            return data;
+        }
+
+        Node pre = head;
+        for (int i = 0; i < index-1; i++) {
+            pre = pre.next;
+        }
+        Node target = pre.next;
+        data = target.getData();
+        pre.next = target.next;
+        return data;
     }
-    
-    public int removeFirst() {
-        return 0;
+
+    public int removeFirst() throws CustomNoSuchElementException{
+        if (size==0 ){
+            throw new CustomNoSuchElementException();
+        }
+
+        int data = head.getData();
+        if(head.next == null){
+            head = null;
+            return data;
+        }
+        head = head.next;
+        return data;
     }
     
     @Override
     public void clear() {
-    
+        head = null;
+        size=0;
     }
 }
+
