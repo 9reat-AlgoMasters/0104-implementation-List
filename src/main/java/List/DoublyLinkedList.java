818894b74eca0920e093ea53dd82bf66e60bf2af
An Na <99172832+An0401na@users.noreply.github.com>
2023-02-26 14:35:31 +0900

    impl: DoublyLinkedList

--- src/main/java/List/DoublyLinkedList.java
+++ src/main/java/List/DoublyLinkedList.java
@@ -1,24 +1,65 @@
 package List;
 
+import exceptions.CustomIndexOutOfBoundsException;
+
+class DNode{
+    private int data;
+    public DNode lNode;
+    public DNode rNode;
+    public DNode(){}
+    public DNode(int data){
+        this.data = data;
+        this.lNode = null;
+        this.rNode = null;
+    }
+    public DNode(DNode lNode, int data, DNode rNode){
+        this.lNode = lNode;
+        this.data = data;
+        this.rNode = rNode;
+    }
+    public int getData() {
+        return data;
+    }
+}
 public class DoublyLinkedList implements List {
+    private int size;
+    private DNode head;
     @Override
     public int size() {
-        return 0;
+        return size;
     }
     
     @Override
     public boolean isEmpty() {
+        if(size ==0){
+            return true;
+        }
         return false;
     }
     
     @Override
     public boolean contains(int num) {
+        DNode n = head;
+        while (!(n == null)){
+            if(num == n.getData()){
+                return true;
+            }else{
+                n = n.rNode;
+            }
+        }
         return false;
     }
     
     @Override
     public int get(int index) {
-        return 0;
+        if (index>=size || index<0){
+            throw new CustomIndexOutOfBoundsException();
+        }
+        DNode n = head;
+        for (int i = 0; i < index; i++) {
+            n = n.rNode;
+        }
+        return n.getData();
     }
     
     public int getFirst() {
